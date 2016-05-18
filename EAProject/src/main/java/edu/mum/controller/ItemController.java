65e59bfccec78b694bc630cb1e3d419a6bf218b6
andres <andres@172.17.5.205>
2016-05-18 01:28:54 -0500

    item controller

--- EAProject/src/main/java/edu/mum/controller/ItemController.java
+++ EAProject/src/main/java/edu/mum/controller/ItemController.java
@@ -1,33 +1,56 @@
 package edu.mum.controller;
 
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomCollectionEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import edu.mum.domain.Category;
 import edu.mum.domain.Item;
+import edu.mum.service.CategoryService;
 import edu.mum.service.ItemService;
 
 @Controller
 @RequestMapping("/items")
 public class ItemController {
-	
+
 	@Autowired
 	private ItemService itemService;
- 
- 	@RequestMapping({"","/all"})
+
+	@Autowired
+	private CategoryService categoryService;
+
+	@InitBinder
+	protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
+		binder.registerCustomEditor(Set.class, new CustomCollectionEditor(Set.class) {
+			@Override
+			protected Object convertElement(Object element) {
+				String categoryId = (String) element;
+				return categoryService.findOne(Long.valueOf(categoryId));
+			}
+		});
+	}
+
+	@RequestMapping({ "", "/all" })
 	public String list(Model model) {
 		model.addAttribute("items", itemService.findAll());
 		return "items";
 	}
-	
- 	@RequestMapping("/item")
+
+	@RequestMapping("/item")
 	public String getItemById(Model model, @RequestParam("id") Long id) {
 
 		Item item = itemService.findOne(id);
@@ -35,27 +58,27 @@ public class ItemController {
 		return "item";
 	}
 
-	
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
-	public String getAddNewItemForm(@ModelAttribute("newItem") Item newItem) {
-	   return "addItem";
+	public String getAddNewItemForm(Model model, @ModelAttribute("newItem") Item newItem) {
+		List<Category> categories = categoryService.findAll();
+		model.addAttribute("categoriesList", categories);
+		return "addItem";
 	}
-	   
+
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public String processAddNewItemForm(@ModelAttribute("newItem") @Valid Item itemToBeAdded, BindingResult result) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return "addItem";
 		}
 
- 		try {
+		try {
 			itemService.update(itemToBeAdded);
 		} catch (Exception up) {
-	      System.out.println("Transaction Failed!!!");
+			System.out.println("Transaction Failed!!!");
 			up.printStackTrace();
 		}
-		
-	   	return "redirect:/items";
+
+		return "redirect:/items";
 	}
-	
-   
+
 }
